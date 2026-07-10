4877d231865f3731de38ee483f6dff1e3dc47ae1
xiaodust <1599112123@qq.com>
2026-07-10 16:12:02 +0800

    增强语音意图识别

--- wx-claw-backfront/src/main/java/com/dust/wxclawbackfront/ai/voice/VoiceIntentDetector.java
+++ wx-claw-backfront/src/main/java/com/dust/wxclawbackfront/ai/voice/VoiceIntentDetector.java
@@ -8,7 +8,10 @@ import java.util.regex.Pattern;
 public class VoiceIntentDetector {
 
     private static final Pattern VOICE_STRICT_PATTERN = Pattern.compile(
-            "(发送语音|发语音|来(一条|一段)?语音|语音(回复|回答)|用语音(回复|回答)|读出来|朗读(一下)?|念出来|播报(一下)?|用(语音|声音)(说|讲)一下|给我(发|来)(一条|一段)?语音)",
+            "(发送语音|发语音|发个语音|来(一条|一段)?语音|语音(回复|回答|告诉我|说)|用语音(回复|回答|告诉我|说)|" +
+            "读出来|朗读(一下)?|念出来|念给我|播报(一下)?|唠一下|说给我听|讲给我听|" +
+            "用(语音|声音|音频)(说|讲|回复|回答)(一下)?|给我(发|来)(一条|一段|个)?(语音|音频)|" +
+            "(语音|音频)(版|形式|方式)|语音跟我说|语音和我说|语音跟我聊)",
             Pattern.CASE_INSENSITIVE);
 
     public boolean isVoiceIntent(String text) {
